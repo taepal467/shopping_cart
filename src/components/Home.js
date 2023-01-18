7d351831c26f4295c32fdba19ef69340318ca0af
Chantae Palacio <chantae.palacio@gmail.com>
2023-01-18 15:08:33 -0800

    Minor changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,4 @@
-import React from 'react';
-
-export default function Home() {
+export function Home() {
     return (
         <div>
             <h1>Home page</h1>
---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+export function Navbar () {
+    return(
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Products</li>
+            </ul>
+        </nav>
+    );
+}
