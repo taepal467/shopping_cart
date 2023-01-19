e2ba5d03ae8a542563f5444c53a93dd956d54003
Chantae Palacio <chantae.palacio@gmail.com>
2023-01-18 17:38:22 -0800

    No changes made

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './components/Home';
 import { Navbar } from './components/Navbar';
-
+import { Shop } from './components/Shop';
 
 function App() {
   return (
-    <div className="App">
-    <Navbar />
-      <Home />
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={ <Navbar /> }>
+          <Route index element={ <Home /> } />
+          <Route path='/shop' element={ <Shop /> } />
+          </Route>
+        </Routes>
+      </BrowserRouter>
   );
 }
 
