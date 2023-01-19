ef01a3e90b0c388d9814fff04bffd46e4c8e2b40
Chantae Palacio <chantae.palacio@gmail.com>
2023-01-18 17:39:41 -0800

    Added the Footer component

---
+++ src/components/Shop.js
@@ -0,0 +1,10 @@
+import { Footer } from "./Footer";
+
+export const Shop = () => {
+    return(
+        <div>
+            <h1>This is the shopping page</h1>
+            <Footer />
+        </div>
+    );
+}
