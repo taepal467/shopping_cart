90bb1b2635455223eb73fedffbb1e14422ad3f2a
Chantae Palacio <chantae.palacio@gmail.com>
2023-01-20 14:22:13 -0800

    Added logo to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,12 @@
 import { Outlet, Link } from "react-router-dom";
+import logo from "../assets/logo.png";
+import "../styles/Navbar.css";
 
 export function Navbar() {
     return(
         <>
         <nav>
+        <img src={logo} alt="Logo" className="logo"/>
             <ul>
                 <li>
                     <Link to='/'>Home</Link>
