a9ec513b0c0e8d69b98eaac1aa0cc3de8be707b5
Chantae Palacio <chantae.palacio@gmail.com>
2023-01-20 14:47:34 -0800

    Added shopping bag button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,21 @@
 import { Outlet, Link } from "react-router-dom";
 import logo from "../assets/logo.png";
+import img from "../assets/bag.png";
 import "../styles/Navbar.css";
 
 export function Navbar() {
     return(
         <>
-        <nav>
-        <img src={logo} alt="Logo" className="logo"/>
-            <ul>
-                <li>
-                    <Link to='/'>Home</Link>
+        <nav className="nav">
+            <img src={logo} alt="Logo" className="logo"/>
+            <ul className="nav--list">
+                <li className="nav--items">
+                    <Link to='/' className="nav--link">Home</Link>
                 </li>
-                <li>
-                    <Link to='/shop'>Shop</Link>
+                <li className="nav--items">
+                    <Link to='/shop' className="nav--link">Shop</Link>
                 </li>
+                <button><img src={img} alt="shopping-bag" className="bag"/></button>
             </ul>
         </nav>
 
