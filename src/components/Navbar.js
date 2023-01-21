54f58b5c1d5c03b2c80f6de5fe0004a89e5816ed
Chantae Palacio <chantae.palacio@gmail.com>
2023-01-20 17:36:59 -0800

    Satisfied with the look of the nav bar for now. Will make changes later on

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import { Outlet, Link } from "react-router-dom";
-import logo from "../assets/logo.png";
+import icon from "../assets/icons8-handbag-64.png";
 import img from "../assets/bag.png";
 import "../styles/Navbar.css";
 
@@ -7,7 +7,11 @@ export function Navbar() {
     return(
         <>
         <nav className="nav">
-            <img src={logo} alt="Logo" className="logo"/>
+            <div className="nav--title">
+                <img src={icon} alt="Logo" className="logo"/>
+                <p className="title">Tae's <span className="span--item">Luxury</span> Discount Store</p>
+            </div>
+         
             <ul className="nav--list">
                 <li className="nav--items">
                     <Link to='/' className="nav--link">Home</Link>
@@ -15,7 +19,7 @@ export function Navbar() {
                 <li className="nav--items">
                     <Link to='/shop' className="nav--link">Shop</Link>
                 </li>
-                <button><img src={img} alt="shopping-bag" className="bag"/></button>
+                <button type="button" className="nav--btn"><img src={img} alt="shopping-cart" className="cart"/></button>
             </ul>
         </nav>
 
