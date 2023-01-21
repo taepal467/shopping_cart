3bdd59151579cc635f6092689ed5c1f4a66331ae
Chantae Palacio <chantae.palacio@gmail.com>
2023-01-20 18:38:05 -0800

    created the layout for the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,54 @@
+import '../styles/Footer.css';
+
 export function Footer() {
     return(
-        <div>
-            <h1>This is the footer</h1>
+        <div className='footer--container'>
+            <div className='newsletter--container'>
+                <form className='form'>
+                    <p>Newsletter</p>
+                    <p>Subscribe to receive news from TLDS</p>
+                    <label for="email"></label>
+                    <input type="email" id="email" placeholder="Enter your email address"></input>
+                    <button type="submit" className='form--btn'>subscribe</button>
+                </form>
+            </div>
+
+            <div className="contact--container">
+                <p>Contact Us</p>
+                <p>TLDS Customer Care is available Monday to Sunday, 7AM to 7PM PT, to answer all your questions. 
+                Please <a href='#'>email us</a>, call <a href="#">1.800.555.5555 </a> or <a href="#">live chat </a>
+                 with a TLDS Advisor.
+                </p>
+            </div>
+
+            <div className='about--container'>
+                    <p>Tae's Luxury Discount Store</p>
+                <ul>
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <a href="#">Careers</a>
+                    </li>
+                    <li>
+                        <a href="#">Stores</a>
+                    </li>
+                    <li>
+                        <a href="#">Follow Us</a>
+                    </li>
+                    <li>
+                        <a href="#">Refund Policy</a>
+                    </li>
+                    <li>
+                        <a href="#">Privacy Policy</a>
+                    </li>
+                    <li>
+                        <a href="#">Do Not Sell or Share My Personal Information</a>
+                    </li>
+                </ul>
+    
+            </div>
+         
         </div>
     );
 }
