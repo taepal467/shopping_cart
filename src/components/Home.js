cf6080312c1a83989b723e6790d19c24b32e826c
Chantae Palacio <chantae.palacio@gmail.com>
2023-01-23 13:34:09 -0800

    Finished working on the Home Page hero section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,23 @@
 import { Footer } from "./Footer"
+import img from "../assets/home-img.avif";
+import "../styles/Home.css";
 
 export function Home() {
     return (
-        <div>
-            <h1>Home page</h1>
-            <Footer />
+        <div className="home--container">
+            <div className="hero">
+                <div className="header--container">
+                    <h1 className="header1">Welcome To Tae's Luxury Discount Store</h1>
+                    <h2 className="header2">Where Everything is 80% off Retail Price!</h2>
+                    <button type="button" className="hero--btn">Shop Now</button>
+                </div>
+                    <div className="img--container">
+                    <img src={img} alt="Gucci Bag" className="hero--img"/>
+                    </div>
+            </div>
+            <footer>
+                <Footer />
+            </footer>
         </div>
     )
 }
