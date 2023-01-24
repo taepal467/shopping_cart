3f3ebb66e510206ce0f05d736895cfe7429c3a4f
Chantae Palacio <chantae.palacio@gmail.com>
2023-01-24 13:43:55 -0800

    Completed the Footer. I had to change the inactive anchor tags to buttons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,54 +1,76 @@
 import '../styles/Footer.css';
+import twitter from "../assets/icons8-twitter-30.png";
+import facebook from "../assets/icons8-facebook-30.png";
+import instagram from "../assets/icons8-instagram-30.png"
 
 export function Footer() {
     return(
         <div className='footer--container'>
-            <div className='newsletter--container'>
-                <form className='form'>
-                    <p>Newsletter</p>
-                    <p>Subscribe to receive news from TLDS</p>
-                    <label for="email"></label>
-                    <input type="email" id="email" placeholder="Enter your email address"></input>
-                    <button type="submit" className='form--btn'>subscribe</button>
-                </form>
+            <div className='footer--content'>
+
+                <div className='newsletter--container'>
+                    <form className='form'>
+                        <p className='header'>Newsletter</p>
+                        <p className='newsletter--content'>Subscribe to receive news from TLDS</p>
+                        <label for="email"></label>
+                        <input type="email" id="email" placeholder="Enter your email address" className='input'></input>
+                        <button type="submit" className='form--btn'>Subscribe</button>
+                    </form>
+                </div>
+
+                <div className="contact--container">
+                    <p className='header'>Contact Us</p>
+                    <p className='contact--content'>TLDS Customer Care is available Monday to Sunday, 7AM to 7PM PT, to answer all your questions. 
+                    Please <button type="button" className='contact--link'>email us</button>, 
+                    call <button type="button" className='contact--link'>1.800.555.5555 </button> or 
+                    <button type="button" className='contact--link live--chat'>    live chat </button>
+                    with a TLDS Advisor.
+                    </p>
+                </div>
+
+                <div className='about--container'>
+                        <p className='header'>Tae's Luxury Discount Store</p>
+                    <ul className='footer--list'>
+                        <li className='list--item'>
+                            <button type="button" className='footer--link'>About</button>
+                        </li>
+                        <li className='list--item'>
+                            <button type="button" className='footer--link'>Careers</button>
+                        </li>
+                        <li className='list--item'>
+                            <button type="button" className='footer--link'>Order Status</button>
+                        </li>
+                        <li className='list--item'>
+                            <button type="button" className='footer--link'>Shipping Policy and Rate</button>
+                        </li>
+                        <li className='list--item'>
+                            <button type="button" className='footer--link'>Returns</button>
+                        </li>
+                        <li className='list--item'>
+                            <button type="button" className='footer--link'>Your Privacy Rights</button>
+                        </li>
+                        <li className='list--item'>
+                            <button type="button" className='footer--link'>Do Not Sell or Share My Personal Information</button>
+                        </li>
+                    </ul>
+                </div>
             </div>
 
-            <div className="contact--container">
-                <p>Contact Us</p>
-                <p>TLDS Customer Care is available Monday to Sunday, 7AM to 7PM PT, to answer all your questions. 
-                Please <a href='#'>email us</a>, call <a href="#">1.800.555.5555 </a> or <a href="#">live chat </a>
-                 with a TLDS Advisor.
-                </p>
+            <div className='icons--container'>
+                <button type="button" className='icon--btn'>
+                    <img src={twitter} alt="twitter"/>
+                </button>
+                <button type="button" className='icon--btn'>
+                    <img src={facebook} alt="facebook"/>
+                </button>
+                <button type="button" className='icon--btn'>
+                    <img src={instagram} alt="instagram"/>
+                </button>
             </div>
-
-            <div className='about--container'>
-                    <p>Tae's Luxury Discount Store</p>
-                <ul>
-                    <li>
-                        <a href="#">About</a>
-                    </li>
-                    <li>
-                        <a href="#">Careers</a>
-                    </li>
-                    <li>
-                        <a href="#">Stores</a>
-                    </li>
-                    <li>
-                        <a href="#">Follow Us</a>
-                    </li>
-                    <li>
-                        <a href="#">Refund Policy</a>
-                    </li>
-                    <li>
-                        <a href="#">Privacy Policy</a>
-                    </li>
-                    <li>
-                        <a href="#">Do Not Sell or Share My Personal Information</a>
-                    </li>
-                </ul>
-    
+            <div className='small--tag'>
+            <small>&copy;2023 Created By <a href="https://github.com/taepal467" target='_blank' rel="noreferrer" className='github--link'>taepal467</a> </small>
             </div>
-         
+        
         </div>
     );
 }
