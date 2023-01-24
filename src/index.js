f32736483ff93ccb7609673d78fa80758fb3288b
Chantae Palacio <chantae.palacio@gmail.com>
2023-01-24 14:43:46 -0800

    Trying to change the default router when launching the web page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './components/Home';
+import { Navbar } from './components/Navbar';
+import { Shop } from './components/Shop';
+
+function App() {
+  return (
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={ <Navbar /> }>
+          <Route index element={ <Home /> } />
+          <Route path='shop' element={ <Shop /> } />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -8,5 +24,7 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+export default App;
 
 
