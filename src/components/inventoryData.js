2519fe6171a4ca62d725418e02a567d30f932b6c
Chantae Palacio <chantae.palacio@gmail.com>
2023-01-27 17:24:46 -0800

    Created the inventoryData.js to store the data information to use for the Shop.js file

--- src/components/inventoryData.js
+++ src/components/inventoryData.js
@@ -1,5 +1,14 @@
-export default [
-    {
+import lv1 from "../assets/lv1.webp";
 
+const data = [
+    {
+        img:{lv1},
+        name:'Alma BB',
+        prices: {
+            firstPrice: '$1,760',
+            secondPrice:'$352'
+        }
     },
 ];
+
+export default data;
