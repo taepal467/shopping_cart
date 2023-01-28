15ae2589c915a1bdff508f53c4ee01903ef2b750
Chantae Palacio <chantae.palacio@gmail.com>
2023-01-27 17:51:10 -0800

    Figuring out the card layout still

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -5,37 +5,37 @@ import img from "../assets/lv1.webp"
 export const Shop = () => {
 
     return(
-        <div>
+        <>
             <div className="inventory--card">
                 <img src={img} className="card--img"/>
-                    <div className="card--info">
-                        <p className="card--item--name">Louis Vuitton Alma BB</p>
+                    <div className="card">
+                        <p className="card--item--name">Alma BB</p>
                         <small className="retail--price">Retail Price: $1760</small>
-                        <small className="tlds--price">TLDS Price: $352</small>
+                        <small className="tlds">TLDS Price: $352</small>
                     </div>
 
                     <img src={img} className="card--img"/>
                     <div className="card--info">
-                        <p className="card--item--name">Louis Vuitton Alma BB</p>
+                        <p className="card--item--name">Alma BB</p>
                         <small className="retail--price">Retail Price: $1760</small>
-                        <small className="tlds--price">TLDS Price: $352</small>
+                        <small className="tlds">TLDS Price: $352</small>
                     </div>
 
                     <img src={img} className="card--img"/>
                     <div className="card--info">
-                        <p className="card--item--name">Louis Vuitton Alma BB</p>
+                        <p className="card--item--name">Alma BB</p>
                         <small className="retail--price">Retail Price: $1760</small>
-                        <small className="tlds--price">TLDS Price: $352</small>
+                        <small className="tlds">TLDS Price: $352</small>
                     </div>
 
                     <img src={img} className="card--img"/>
                     <div className="card--info">
-                        <p className="card--item--name">Louis Vuitton Alma BB</p>
+                        <p className="card--item--name">Alma BB</p>
                         <small className="retail--price">Retail Price: $1760</small>
-                        <small className="tlds--price">TLDS Price: $352</small>
+                        <small className="tlds">TLDS Price: $352</small>
                     </div>
             </div>
             <Footer />
-        </div>
+        </>
     );
 }
