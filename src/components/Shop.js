d407c681f36a9d1cd8117d92dfc259e7317601c3
Chantae Palacio <chantae.palacio@gmail.com>
2023-01-27 17:23:49 -0800

    Hard coding the card details to see how things will look

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,9 +1,40 @@
 import { Footer } from "./Footer";
+import '../styles/Shop.css';
+import img from "../assets/lv1.webp"
 
 export const Shop = () => {
+
     return(
         <div>
-            <h1>This is the shopping page</h1>
+            <div className="inventory--card">
+                <img src={img} className="card--img"/>
+                    <div className="card--info">
+                        <p className="card--item--name">Louis Vuitton Alma BB</p>
+                        <small className="retail--price">Retail Price: $1760</small>
+                        <small className="tlds--price">TLDS Price: $352</small>
+                    </div>
+
+                    <img src={img} className="card--img"/>
+                    <div className="card--info">
+                        <p className="card--item--name">Louis Vuitton Alma BB</p>
+                        <small className="retail--price">Retail Price: $1760</small>
+                        <small className="tlds--price">TLDS Price: $352</small>
+                    </div>
+
+                    <img src={img} className="card--img"/>
+                    <div className="card--info">
+                        <p className="card--item--name">Louis Vuitton Alma BB</p>
+                        <small className="retail--price">Retail Price: $1760</small>
+                        <small className="tlds--price">TLDS Price: $352</small>
+                    </div>
+
+                    <img src={img} className="card--img"/>
+                    <div className="card--info">
+                        <p className="card--item--name">Louis Vuitton Alma BB</p>
+                        <small className="retail--price">Retail Price: $1760</small>
+                        <small className="tlds--price">TLDS Price: $352</small>
+                    </div>
+            </div>
             <Footer />
         </div>
     );
