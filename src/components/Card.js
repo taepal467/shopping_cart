c5c457633cd9561c6075477b01111a21a9742566
Chantae Palacio <chantae.palacio@gmail.com>
2023-01-28 16:59:17 -0800

    Finally got the props to read correctly

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,14 @@
 import '../styles/Card.css';
 
 export default function Card(props) {
-    console.log("Card Component: ", props)
     return (
-        <div className="inventory--card">
-        <img src={`../assets/${props.img}`} className="card--img" alt="handbag"/>
+        <div className="card">
+        <img src={props.img} className="card--img" alt={props.name}/>
             <div className="card--info">
                 <p className="card--item--name">{props.name}</p>
-                <small className="retail--price">{props.firstPrice}</small>
-                <small className="tlds--price">{props.secondPrice}</small>
+                <small className="retail--price">Retail:$ {props.prevPrice}</small>
+                <small className="tlds--price">TLDS Price:$ {props.currentPrice}</small>
             </div>
-    </div>
+        </div>
     )
 }
