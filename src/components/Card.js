2c59c42b7a1cd6334d527e76ff30e1899caeebb7
Chantae Palacio <chantae.palacio@gmail.com>
2023-01-30 14:19:02 -0800

    Since i am passing in an oject as a prop, had to add item to each prop

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,12 +3,12 @@ import '../styles/Card.css';
 export default function Card(props) {
     return (
         <div className="card">
-        <img src={props.img} className="card--img" alt={props.brandName}/>
+        <img src={props.item.img} className="card--img" alt={props.item.brandName}/>
             <div className="card--info">
-                <p className="card--item--brand">{props.brandName}</p>
-                <p className="card--item--handbag">{props.handbag}</p>
-                <p className="retail--price">Retail: ${props.prevPrice}</p>
-                <p className="tlds--price">TLDS Price: ${props.currentPrice}</p>
+                <p className="card--item--brand">{props.item.brandName}</p>
+                <p className="card--item--handbag">{props.item.handbag}</p>
+                <p className="retail--price">Retail: ${props.item.prevPrice}</p>
+                <p className="tlds--price">TLDS Price: ${props.item.currentPrice}</p>
             </div>
         </div>
     )
