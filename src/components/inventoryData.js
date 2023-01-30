028a29ff98edf94704ff902bd6c685c6403928fd
Chantae Palacio <chantae.palacio@gmail.com>
2023-01-30 11:02:21 -0800

    Added the 5 louis vuitton images

--- src/components/inventoryData.js
+++ src/components/inventoryData.js
@@ -1,29 +1,51 @@
-// import lv1 from "../images/lv1.webp";
+// Louis Vuitton
 import louisV1 from "../images/lv1.webp"
-import louisV2 from "../images/lv1.webp"
-import louisV3 from "../images/lv1.webp"
+import louisV2 from "../images/lv2.webp"
+import louisV3 from "../images/lv3.png"
+import louisV4 from "../images/lv4.webp"
+import louisV5 from "../images/lv5.webp"
+
 
 const data =  [
     {
         id: 1,
         img:louisV1,
-        name:'Louis Vuitton Alma BB',
+        brandName:"Louis Vuitton",
+        handbag: "Alma BB",
         prevPrice: 1760,
         currentPrice: 352
     },
     {
         id: 2,
         img:louisV2,
-        name:'Louis Vuitton Mini Backpack',
-        prevPrice: 4760,
-        currentPrice: 1352
+        brandName:"Louis Vuitton",
+        handbag: "Speedy Bandouliere 20",
+        prevPrice: 2570,
+        currentPrice: 514
     },
     {
         id: 3,
         img:louisV3,
-        name:'Louis Vuitton Belt',
-        prevPrice: 500,
-        currentPrice: 100
+        brandName:"Louis Vuitton",
+        handbag:"LV X YK Neverfull MM",
+        prevPrice: 2980,
+        currentPrice: 596
+    },
+    {
+        id: 4,
+        img:louisV4,
+        brandName:"Louis Vuitton",
+        handbag:"CarryAll PM",
+        prevPrice: 3200,
+        currentPrice: 640
+    },
+    {
+        id: 5,
+        img:louisV5,
+        brandName:"Louis Vuitton ",
+        handbag:"Palm Springs Mini",
+        prevPrice: 2440,
+        currentPrice: 488
     },
 ];
 
