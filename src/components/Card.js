bb5ceccf273ea56933865776edbdcb42a8f611af
Chantae Palacio <chantae.palacio@gmail.com>
2023-01-30 11:00:59 -0800

    Changed card layout information

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,11 +3,12 @@ import '../styles/Card.css';
 export default function Card(props) {
     return (
         <div className="card">
-        <img src={props.img} className="card--img" alt={props.name}/>
+        <img src={props.img} className="card--img" alt={props.brandName}/>
             <div className="card--info">
-                <p className="card--item--name">{props.name}</p>
-                <small className="retail--price">Retail:$ {props.prevPrice}</small>
-                <small className="tlds--price">TLDS Price:$ {props.currentPrice}</small>
+                <p className="card--item--brand">{props.brandName}<span></span></p>
+                <p className="card--item--handbag">{props.handbag}</p>
+                <p className="retail--price">Retail:$ {props.prevPrice}</p>
+                <p className="tlds--price">TLDS Price:$ {props.currentPrice}</p>
             </div>
         </div>
     )
