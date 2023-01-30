25d805288c517129cc8f0aa3897a60632c12fad3
Chantae Palacio <chantae.palacio@gmail.com>
2023-01-30 11:19:51 -0800

    Added 5 chanel images

--- src/components/inventoryData.js
+++ src/components/inventoryData.js
@@ -5,6 +5,20 @@ import louisV3 from "../images/lv3.png"
 import louisV4 from "../images/lv4.webp"
 import louisV5 from "../images/lv5.webp"
 
+//Chanel
+import chanel1 from "../images/chanel1.webp"
+import chanel2 from "../images/chanel2.webp"
+import chanel3 from "../images/chanel3.webp"
+import chanel4 from "../images/chanel4.webp"
+import chanel5 from "../images/chanel5.webp"
+
+//Birkin
+import berkin1 from "../images/birkin1.webp"
+import berkin2 from "../images/birkin2.webp"
+import berkin3 from "../images/birkin3.webp"
+import berkin4 from "../images/birkin4.webp"
+import berkin5 from "../images/birkin5.webp"
+
 
 const data =  [
     {
@@ -47,6 +61,47 @@ const data =  [
         prevPrice: 2440,
         currentPrice: 488
     },
+    {
+        id: 6,
+        img:chanel1,
+        brandName:"Chanel",
+        handbag:"Small Backpack",
+        prevPrice: 5000,
+        currentPrice: 1000
+    },
+    {
+        id: 7,
+        img:chanel2,
+        brandName:"Chanel",
+        handbag:"Classic Handbag",
+        prevPrice: 8100,
+        currentPrice: 1620
+    },
+    {
+        id: 8,
+        img:chanel3,
+        brandName:"Chanel",
+        handbag:"Large Shopping Bag",
+        prevPrice: 4600,
+        currentPrice: 920
+    },
+    {
+        id: 9,
+        img:chanel4,
+        brandName:"Chanel",
+        handbag:"Mini Camera Case",
+        prevPrice: 4600,
+        currentPrice: 920
+    },
+    {
+        id: 10,
+        img:chanel5,
+        brandName:"Chanel",
+        handbag:"Chanel 22 Handbag",
+        prevPrice: 5000,
+        currentPrice: 1000
+    },
+    
 ];
 
 export default data;
