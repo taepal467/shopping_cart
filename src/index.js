a15e721e02ce4a5e5770d71bb1a4ebd3f3a10e81
Chantae Palacio <chantae.palacio@gmail.com>
2023-02-01 13:55:15 -0800

    Added a Route for the Cart link

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './components/Home';
 import { Navbar } from './components/Navbar';
 import { Shop } from './components/Shop';
+import {Cart} from './components/Cart'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path='/' element={ <Navbar /> }>
           <Route index element={ <Home /> } />
           <Route path='/shop' element={ <Shop /> } />
+          <Route path='/cart' element={ <Cart /> } />
           </Route>
         </Routes>
       </BrowserRouter>
