3f6a7ccbf18247ff14233dbe43d3e5106452efc6
Chantae Palacio <chantae.palacio@gmail.com>
2023-02-01 16:29:25 -0800

    Props are not passing through to the Cart component. The add button works. But the item props are not reading

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,9 +1,27 @@
 export function Cart (props) {
-    console.log("cart component props: ", props)
+    const {cartItems, onAddItem, onRemoveItem} = props;
+    console.log("cart component props: ", cartItems)
     return(
         <div>
-            <h1>Cart is Empty!!!!!!!</h1>
+            <h1>Cart Items</h1>
+            <div>{cartItems.length === 0 && <div>Cart is Empty</div>}</div>
+            {cartItems.map(item => (
+     
+                <div key={item.id}>
+                    <div>{item.brandName}</div>
+                    <div>
+                        <button onClick={() => onAddItem(item)}>+</button>
+                        <button onClick={() => onRemoveItem(item)}>-</button>
+                    </div>
+                    <div>
+                        {item.qty} x $ {console.log(item.currentPrice)}
+                    </div>
+                </div>
+            ))}
         </div>
     );
 }
 
+// 02/01 -- the "item" Props are not passing through.*Face Palm* 
+//The add button works
+// need to create the onRemoveItem function
