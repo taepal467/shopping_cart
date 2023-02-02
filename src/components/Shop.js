9533a5d2069090ee091332236107c8bdfba40f52
Chantae Palacio <chantae.palacio@gmail.com>
2023-02-01 16:26:45 -0800

    Passed the onAdItems as a prop to the Shop component to pass to the Card component to pass to the Cart component which is not workiing

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,16 +1,20 @@
 import { Footer } from "./Footer";
 import data from "../components/inventoryData";
 import Card from "./Card";
+import { useState } from "react";
 
 
-export const Shop = () => {
- 
-const cards = data.map(item => {
-    return <Card 
-        key={item.id}
-        item={item}
-    />
-})
+export const Shop = (props) => {
+    const { onAddItem } = props;
+
+    const cards = data.map(item => {
+        return <Card 
+            key={item.id}
+            item={item}
+            onAddItem={onAddItem}
+        />
+    })
+
     return(
         <div>
              <section className="cards-list">
