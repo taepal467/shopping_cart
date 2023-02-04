0e0d0c97d92a78d63b2d9fafbcdf6195f4ec6036
Chantae Palacio <chantae.palacio@gmail.com>
2023-02-03 17:47:27 -0800

    Removed the Cart import file

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,19 +1,16 @@
 import { Footer } from "./Footer";
 import Card from "./Card";
-import {Cart} from "./Cart"
 
 export const Shop = (props) => {
     const { data, onAddItem, onRemoveItem } = props;
 
     const cards = data.map(item => {
-        return (
-            <Card 
+        return <Card 
                 key={item.id}
                 item={item}
                 onAddItem={onAddItem}
                 onRemoveItem={onRemoveItem}
-            />
-        )
+            />  
     })
 
     return(
