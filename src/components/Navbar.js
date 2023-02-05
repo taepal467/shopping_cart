604824315a2310db3cfee2483cea9a120edded9b
Chantae Palacio <chantae.palacio@gmail.com>
2023-02-04 18:05:13 -0800

    Used ChatGPT to create a hook for using a fixed Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,15 +2,33 @@ import { Outlet, Link } from "react-router-dom";
 import icon from "../assets/icons8-handbag-64.png";
 import img from "../assets/bag.png";
 import "../styles/Navbar.css";
+import React, { useState } from "react";
 
 export function Navbar(props) {
-
     const {countCartItems} = props;
-    console.log("count cart items: ",props)
+    const [isFixed, setIsFixed] = useState(false);
+
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsFixed(true);
+      } else {
+        setIsFixed(false);
+      }
+    };
+  
+    React.useEffect(() => {
+      window.addEventListener("scroll", handleScroll);
+  
+      return () => {
+        window.removeEventListener("scroll", handleScroll);
+      };
+    }, []);
+
+    const navbarClass = isFixed ? "fixed" : "";
 
     return(
-        <>
-        <nav className="nav">
+        <header >
+        <nav className={` nav navbar ${navbarClass}`}>
             <div className="nav--title">
                 <img src={icon} alt="Logo" className="logo"/>
                 <p className="title">Tae's <span className="span--item">Luxury</span> Discount Store</p>
@@ -24,6 +42,7 @@ export function Navbar(props) {
                     <Link to='/shop' className="nav--link">Shop</Link>
                 </li>
        
+                <li className="nav--items">
                 <Link to='/cart'>
                     {countCartItems ? (
                         <button type="button" className="count--btn">{countCartItems }</button>
@@ -34,10 +53,11 @@ export function Navbar(props) {
                         <img src={img} alt="shopping-cart" className="cart"/>
                     </button>
                 </Link>
+                </li>
             </ul>
         </nav>
 
         <Outlet />
-        </>
+        </header>
     );
 }
